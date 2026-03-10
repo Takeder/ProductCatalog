7297e2fa261020d32c93a384189317f6b2e340c6
Takeder <s-kravchenko100@mail.ru>
2026-03-11 01:50:29 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { type Product } from "./types/product";
+import { ProductCard } from "./components/ProductCard/ProductCard";
+import styles from "./components/ProductCard/ProductCard.module.css";
+
+const API_URL = "https://api.escuelajs.co/api/v1/products";
+
+const App: React.FC = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const loadData = async () => {
+            try {
+                const res = await fetch(API_URL);
+                if (!res.ok) throw new Error("Ошибка сети");
+                const data: Product[] = await res.json();
+                setProducts(data);
+            } catch (err) {
+                setError(
+                    err instanceof Error ? err.message : "Ошибка загрузки",
+                );
+            }
+        };
+        loadData();
+    }, []);
+
+    if (error) return <div>Ошибка: {error}</div>;
+
+    return (
+        <main className={styles.grid}>
+            {products.map((item) => (
+                <ProductCard key={item.id} product={item} />
+            ))}
+        </main>
+    );
+};
+
+export default App;
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,23 @@
+import { type Product } from "../../types/product";
+import styles from "./ProductCard.module.css"; // Импорт стилей
+
+interface ProductCardProps {
+    product: Product;
+}
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+    const imageUrl = product.images[0];
+
+    return (
+        <article className={styles.card}>
+            <img src={imageUrl} alt={product.title} className={styles.image} />
+            <span className={styles.category}>{product.category.name}</span>
+            <h3 className={styles.title}>{product.title}</h3>
+
+            <div className={styles.footer}>
+                <span className={styles.price}>${product.price}</span>
+                <button className={styles.button}>Купить</button>
+            </div>
+        </article>
+    );
+};
