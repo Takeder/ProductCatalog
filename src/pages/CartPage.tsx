8981d1924f814b5244ea76594ba23981824ae9bb
sanykos <sanykos446@gmail.com>
2026-03-19 21:14:51 +0300

    test

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,9 +1,9 @@
 import { useAppSelector, useAppDispatch } from "../store/store";
 // Импортируем addToCart (для плюса) и decrementQuantity (для минуса)
 import {
-    removeFromCart,
-    addToCart,
-    decrementQuantity,
+  removeFromCart,
+  addToCart,
+  decrementQuantity,
 } from "../store/reducers/cartSlice";
 import { Link } from "react-router-dom";
 import styles from "./CartPage.module.css";
@@ -11,77 +11,69 @@ import styles from "./CartPage.module.css";
 // Страница корзины
 
 export const CartPage = () => {
-    const { items } = useAppSelector((state) => state.cart); // Достаем список товаров из корзины
-    const dispatch = useAppDispatch();
-    const total = items.reduce((sum, i) => sum + i.price * i.quantity, 0); // Считаем общую сумму: (цена * количество) для каждой позиции
+  const { items } = useAppSelector((state) => state.cart); // Достаем список товаров из корзины
+  const dispatch = useAppDispatch();
+  const total = items.reduce((sum, i) => sum + i.price * i.quantity, 0); // Считаем общую сумму: (цена * количество) для каждой позиции
 
-    return (
-        <div className={styles.container}>
-            <h1>Корзина</h1>
-            {items.length === 0 ? (
-                <p>
-                    Пусто.
-                    <Link to="/" className={styles.back}>
-                        В магазин
-                    </Link>
-                </p>
-            ) : (
-                <>
-                    {items.map((item) => (
-                        <div key={item.id} className={styles.item}>
-                            <div>
-                                <span className={styles.infoName}>
-                                    {item.title}
-                                </span>
+  console.log("tst-branch");
 
-                                {/* КНОПКИ СЧЕТЧИКА */}
-                                <div className={styles.counter}>
-                                    <button
-                                        className={styles.countBtn}
-                                        onClick={() =>
-                                            dispatch(decrementQuantity(item.id))
-                                        }
-                                    >
-                                        -
-                                    </button>
+  return (
+    <div className={styles.container}>
+      <h1>Корзина</h1>
+      {items.length === 0 ? (
+        <p>
+          Пусто.
+          <Link to="/" className={styles.back}>
+            В магазин
+          </Link>
+        </p>
+      ) : (
+        <>
+          {items.map((item) => (
+            <div key={item.id} className={styles.item}>
+              <div>
+                <span className={styles.infoName}>{item.title}</span>
 
-                                    <span className={styles.quantity}>
-                                        {item.quantity}
-                                    </span>
+                {/* КНОПКИ СЧЕТЧИКА */}
+                <div className={styles.counter}>
+                  <button
+                    className={styles.countBtn}
+                    onClick={() => dispatch(decrementQuantity(item.id))}
+                  >
+                    -
+                  </button>
 
-                                    <button
-                                        className={styles.countBtn}
-                                        onClick={() =>
-                                            dispatch(addToCart(item))
-                                        }
-                                    >
-                                        +
-                                    </button>
-                                </div>
+                  <span className={styles.quantity}>{item.quantity}</span>
 
-                                <span className={styles.infoMeta}>
-                                    Цена: ${item.price * item.quantity}
-                                </span>
-                            </div>
+                  <button
+                    className={styles.countBtn}
+                    onClick={() => dispatch(addToCart(item))}
+                  >
+                    +
+                  </button>
+                </div>
 
-                            <button
-                                className={styles.removeBtn}
-                                onClick={
-                                    () => dispatch(removeFromCart(item.id)) // Кнопка удаления всей позиции
-                                }
-                            >
-                                Удалить
-                            </button>
-                        </div>
-                    ))}
-                    <div className={styles.total}>
-                        Итого: ${total.toFixed(2)}
-                    </div>
-                    <Link to="/" className={styles.back}>
-                        ← Назад к покупкам
-                    </Link>
-                </>
-            )}
-        </div>
-    );
+                <span className={styles.infoMeta}>
+                  Цена: ${item.price * item.quantity}
+                </span>
+              </div>
+
+              <button
+                className={styles.removeBtn}
+                onClick={
+                  () => dispatch(removeFromCart(item.id)) // Кнопка удаления всей позиции
+                }
+              >
+                Удалить
+              </button>
+            </div>
+          ))}
+          <div className={styles.total}>Итого: ${total.toFixed(2)}</div>
+          <Link to="/" className={styles.back}>
+            ← Назад к покупкам
+          </Link>
+        </>
+      )}
+    </div>
+  );
 };
